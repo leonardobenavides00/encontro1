d71df1ed3440e9901d20aaecad14c7a0616d8127
leonardobenavides <leonardobenavides2@gmail.com>
2022-06-18 18:09:10 -0500

    Adiciona a atividade do encontro remoto

---
+++ variaveis.js
@@ -0,0 +1,12 @@
+let idade = 2;
+let idadeMeses = idade * 12;
+let idadeDias = idade * 365;
+let idadeHoras = idadeDias  * 24;
+let idadeMinutos = idadeHoras * 60;
+console.log ("Sua idade em meses é " + idadeMeses);
+console.log ("Sua idade em dias é " + idadeDias);
+console.log ("Sua idade em horas é " + idadeHoras);
+console.log ("Sua idade em minutos é " + idadeMinutos);
+
+
+
